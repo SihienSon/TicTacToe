26edabbd8752db7e916f2a4e37506b7878c48fc1
Sihien Son <Sihienkson@Sihiens-MacBook-Pro.local>
2020-02-13 20:44:46 -0700

    Added DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,3 +13,13 @@ void TicTacToe::CreateBoard()
         }
     }
 }
+
+void TicTacToe::DisplayBoard(){
+  for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++){
+          cout << Array[i][j] << " ";
+        }
+      cout << endl;
+    }
+}
